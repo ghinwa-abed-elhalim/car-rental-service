9a5992aa98366667bd77bc66beeaaf5314460278
ghinwa-abedelhalim <ghenwaah77@gmail.com>
2025-03-01 23:08:24 +0000

    class vehicle added

---
+++ car-rental-service.py
@@ -0,0 +1,7 @@
+class vehicle: 
+    def __init__(self, brand, model, year, rental_price_per_days):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.rental_price_per_days = rental_price_per_days
+        
